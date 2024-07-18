fb861c6c9b22823f4994cca847b9aee40bcbd554
kjb1045 <kjb1045@naver.com>
2024-07-18 17:58:14 +0900

    netflex-data

---
+++ src/data.js
@@ -0,0 +1,15 @@
+[
+    {
+        id : 11,
+        category : 2,
+        title : " ",
+        poster : " ",
+        age : " ",
+        startYear : " ",
+        story : " ",
+        movieTime : " ",
+        cast : "a,b,c",
+        director : " ",
+        // video : ''
+    }
+]
