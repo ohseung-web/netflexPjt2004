d14e29ece514604245c2e0e4d27c66f1ed8add71
yuna <illil@naver.com>
2024-07-30 11:50:54 +0900

    components uploads test

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import './App.css';
 import React from 'react';
-import Test from './pages/test';
+// import Test from './pages/test';
+//컴포넌트 loads
+import Header from './components/Header';
+import HeroBanner from './components/HeroBanner';
+import Row from './components/Row';
+import MovieCard from './components/MovieCard';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <>
-      <Test />
+      <Header />
+      <HeroBanner />
+      <Row />
+      <MovieCard />
+      <Footer />
     </>
   );
 }
---
+++ src/components/Row.js
@@ -0,0 +1,5 @@
+export default function Row(){
+    return(<>
+        row row row
+    </>)
+}
